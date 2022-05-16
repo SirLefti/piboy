bf95a7a9479d336f8f9a4f33a361e61858502609
SirLefti <thorben.yzer@web.de>
2022-05-16 21:18:45 +0200

    class for SPI device config

--- config.py
+++ config.py
@@ -1,10 +1,30 @@
 from PIL import ImageFont
 
 
-# Display definition (SPI display module ignores this, it supports only its own resolution)
-WIDTH = 480
-HEIGHT = 320
+class SpiConfig:
+    """Representation of an SPI device configuration"""
+    def __init__(self, bus: int, device: int):
+        self.__bus = bus
+        self.__device = device
+
+    @property
+    def bus(self) -> int:
+        return self.__bus
+
+    @property
+    def device(self) -> int:
+        return self.__device
+
+
+# Interface definition (SPI display module ignores this, it supports only its own resolution)
+WIDTH: int = 480
+HEIGHT: int = 320
 RESOLUTION = WIDTH, HEIGHT
+
+# App definition
+APP_SIDE_OFFSET: int = 20
+APP_TOP_OFFSET: int = 30
+APP_BOTTOM_OFFSET: int = 25
 
 # Fonts
 FONT_NAME = 'FreeSansBold.ttf'
@@ -16,14 +36,12 @@ BACKGROUND = (0, 0, 0)
 ACCENT = (27, 251, 30)
 ACCENT_DARK = (9, 64, 9)
 
-# Pin definition
+# Display module definition
 RST_PIN = 25
 DC_PIN = 24
-# SPI definition
-SPI_BUS = 0
-SPI_DEVICE = 0
+DISPLAY_CONFIG = SpiConfig(0, 0)
 
-# App definition
-APP_SIDE_OFFSET: int = 20
-APP_TOP_OFFSET: int = 30
-APP_BOTTOM_OFFSET: int = 25
+# Touch module definition
+CS_PIN = 7
+IRQ_PIN = 17
+TOUCH_CONFIG = SpiConfig(0, 1)
