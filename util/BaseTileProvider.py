d57958b5a9fd70848addfdb1c8c6d67f43c61d67
Thorben Yzer <thorben.yzer@outlook.com>
2022-05-28 20:20:18 +0200

    map marker of current location

--- util/BaseTileProvider.py
+++ util/BaseTileProvider.py
@@ -3,7 +3,27 @@ from typing import Tuple
 from PIL import Image
 
 
+class TileInfo:
+
+    def __init__(self, top_left: Tuple[float, float], bottom_right: Tuple[float, float], image: Image):
+        self.__top_left = top_left
+        self.__bottom_right = bottom_right
+        self.__image = image
+
+    @property
+    def top_left(self) -> Tuple[float, float]:
+        return self.__top_left
+
+    @property
+    def bottom_right(self) -> Tuple[float, float]:
+        return self.__bottom_right
+
+    @property
+    def image(self) -> Image:
+        return self.__image
+
+
 class BaseTileProvider(ABC):
 
-    def get_tile(self, lat: float, lon: float, zoom: int, size: Tuple[int, int] = (256, 256)) -> Image:
+    def get_tile(self, lat: float, lon: float, zoom: int, size: Tuple[int, int] = (256, 256)) -> TileInfo:
         raise NotImplementedError
--- util/OSMTileProvider.py
+++ util/OSMTileProvider.py
@@ -1,4 +1,4 @@
-from util.BaseTileProvider import BaseTileProvider
+from util.BaseTileProvider import BaseTileProvider, TileInfo
 from typing import Tuple
 from PIL import Image
 import os
@@ -8,10 +8,10 @@ import math
 
 class OSMTileProvider(BaseTileProvider):
 
-    def get_tile(self, lat: float, lon: float, zoom: int, size: Tuple[int, int] = (256, 256)) -> Image:
+    def get_tile(self, lat: float, lon: float, zoom: int, size: Tuple[int, int] = (256, 256)) -> TileInfo:
         tile_cache = '.tiles'
         cache_template = '{zoom}-{x}-{y}.png'
-        x_tile, y_tile = self._deg2num(lat, lon, zoom)
+        x_tile, y_tile = self._deg_to_num(lat, lon, zoom)
 
         if not os.path.exists(tile_cache):
             os.mkdir(tile_cache)
@@ -19,14 +19,16 @@ class OSMTileProvider(BaseTileProvider):
         tile_path = os.path.join(tile_cache, cache_template.format(zoom=zoom, x=x_tile, y=y_tile))
         if os.path.exists(tile_path):
             img = Image.open(tile_path)
-            return self._resize(img, size)
+            return TileInfo(self._num_to_deg(x_tile, y_tile, zoom), self._num_to_deg(x_tile + 1, y_tile + 1, zoom),
+                            self._resize(img, size))
         else:
             response = requests.get(f'https://tile.openstreetmap.org/{zoom}/{x_tile}/{y_tile}.png')
             if response.status_code == 200:
                 with open(tile_path, 'wb') as f:
                     f.write(response.content)
                 img = Image.open(tile_path)
-                return self._resize(img, size)
+                return TileInfo(self._num_to_deg(x_tile, y_tile, zoom), self._num_to_deg(x_tile + 1, y_tile + 1, zoom),
+                                self._resize(img, size))
             else:
                 raise ValueError(f'Fetching OSM tile failed ({response.status_code})')
 
@@ -44,10 +46,19 @@ class OSMTileProvider(BaseTileProvider):
         return resized.crop((offset_x, offset_y, offset_x + new_x, offset_y + new_y))
 
     @classmethod
-    def _deg2num(cls, lat_deg, lon_deg, zoom):
+    def _deg_to_num(cls, lat_deg: float, lon_deg: float, zoom: int) -> Tuple[int, int]:
         """Code from: https://wiki.openstreetmap.org/wiki/Slippy_map_tilenames"""
         lat_rad = math.radians(lat_deg)
         n = 2.0 ** zoom
         x_tile = int((lon_deg + 180.0) / 360.0 * n)
         y_tile = int((1.0 - math.asinh(math.tan(lat_rad)) / math.pi) / 2.0 * n)
         return x_tile, y_tile
+
+    @classmethod
+    def _num_to_deg(cls, x_tile: int, y_tile: int, zoom: int) -> Tuple[float, float]:
+        """Code from: https://wiki.openstreetmap.org/wiki/Slippy_map_tilenames"""
+        n = 2.0 ** zoom
+        lon_deg = x_tile / n * 360.0 - 180.0
+        lat_rad = math.atan(math.sinh(math.pi * (1 - 2 * y_tile / n)))
+        lat_deg = math.degrees(lat_rad)
+        return lat_deg, lon_deg
