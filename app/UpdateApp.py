b824d04c1e1033a499e2d088ef925a3f8b9d59ef
SirLefti <62506842+SirLefti@users.noreply.github.com>
2023-05-19 14:20:10 +0200

    showing option results, added to main scripts

--- app/UpdateApp.py
+++ app/UpdateApp.py
@@ -1,18 +1,21 @@
 from app.App import App
 from PIL import Image, ImageDraw
-from subprocess import run, CompletedProcess
+from subprocess import run, CompletedProcess, PIPE
 from typing import Callable, Optional
 import config
 
 
 class UpdateApp(App):
     LINE_HEIGHT = 20
+    CENTER_OFFSET = 10
 
     class Option:
 
-        def __init__(self, name: str, action: Callable[[], Optional[CompletedProcess]]):
+        def __init__(self, name: str, action: Callable[[], Optional[CompletedProcess]],
+                     count_action: Callable[[], Optional[int]] = None):
             self.__name = name
             self.__action = action
+            self.__count_action = count_action
 
         @property
         def name(self) -> str:
@@ -22,37 +25,72 @@ class UpdateApp(App):
         def action(self) -> Callable[[], Optional[CompletedProcess]]:
             return self.__action
 
+        @property
+        def count_action(self) -> Callable[[], Optional[int]]:
+            return self.__count_action
+
     def __init__(self):
         self.__selected_index = 0
+        self.__files_to_reset: Optional[int] = None
+        self.__files_to_clean: Optional[int] = None
+
+        def get_files_to_reset():
+            return self.__files_to_reset
+
+        def get_files_to_clean():
+            return self.__files_to_clean
+
         self.__options = [
             self.Option('fetch updates', self.__run_fetch),
-            self.Option('reset changes', self.__run_reset),
-            self.Option('clean files', self.__run_clean),
+            self.Option('reset changes', self.__run_reset, count_action=get_files_to_reset),
+            self.Option('clean files', self.__run_clean, count_action=get_files_to_clean),
             self.Option('install updates', self.__run_install),
             self.Option('restart', self.__run_restart)
         ]
         self.__result = None
 
     @staticmethod
-    def __run_fetch():
-        return run(['git', 'fetch'])
+    def __run_fetch() -> CompletedProcess:
+        return run(['git', 'fetch'], stdout=PIPE)
 
     @staticmethod
-    def __run_reset():
-        return run(['git', 'reset', '--hard'])
+    def __run_reset() -> CompletedProcess:
+        return run(['git', 'reset', '--hard'], stdout=PIPE)
 
     @staticmethod
-    def __run_clean():
-        return run(['git', 'clean', '-fd'])
+    def __run_clean() -> CompletedProcess:
+        return run(['git', 'clean', '-fd'], stdout=PIPE)
 
     @staticmethod
-    def __run_install():
-        return run(['git', 'pull'])
+    def __run_install() -> CompletedProcess:
+        return run(['git', 'pull'], stdout=PIPE)
 
     @staticmethod
-    def __run_restart():
+    def __run_restart() -> CompletedProcess:
         # TODO implement
         pass
+
+    @staticmethod
+    def __get_files_to_reset() -> Optional[int]:
+        result = run(['git', 'diff', '--name-only'], stdout=PIPE)
+        if result.returncode == 0:
+            if result.stdout is None:
+                return 0
+            else:
+                return result.stdout.decode('utf-8').count('\n')
+        else:
+            return None
+
+    @staticmethod
+    def __get_files_to_clean() -> Optional[int]:
+        result = run(['git', 'clean', '-nd'], stdout=PIPE)
+        if result.returncode == 0:
+            if result.stdout is None:
+                return 0
+            else:
+                return result.stdout.decode('utf-8').count('\n')
+        else:
+            return None
 
     @property
     def title(self) -> str:
@@ -63,11 +101,17 @@ class UpdateApp(App):
         font = config.FONT_STANDARD
 
         left_top = (config.APP_SIDE_OFFSET, config.APP_TOP_OFFSET)
+        right_bottom = (width / 2, config.APP_TOP_OFFSET)
         draw = ImageDraw.Draw(image)
 
         if self.__result is not None:
-            print(self.__result.stdout)
-            print(self.__result.returncode)
+            # log action responses
+            text = self.__result.stdout.decode('utf-8') if self.__result.stdout else f'code: {self.__result.returncode}'
+            _, _, _, text_height = font.getbbox(text)
+            log_left_top = (width / 2 + self.CENTER_OFFSET, left_top[1])
+            right_bottom = (width - config.APP_SIDE_OFFSET, left_top[1] + text_height)
+            draw.rectangle(log_left_top + right_bottom, fill=config.BACKGROUND)
+            draw.text(log_left_top, text, fill=config.ACCENT, font=font)
             self.__result = None
 
         cursor = left_top
@@ -76,12 +120,20 @@ class UpdateApp(App):
                 draw.rectangle(cursor + (width / 2, cursor[1] + self.LINE_HEIGHT), fill=config.ACCENT_DARK)
             else:
                 draw.rectangle(cursor + (width / 2, cursor[1] + self.LINE_HEIGHT), fill=config.BACKGROUND)
-            draw.text(cursor, option.name, fill=config.ACCENT, font=font)
+            text = option.name
+            if option.count_action is not None:
+                count = option.count_action()
+                if count is not None:
+                    text = f'{text} ({count} files)'
+                else:
+                    text = f'{text} (error)'
+            draw.text(cursor, text, fill=config.ACCENT, font=font)
             cursor = (cursor[0], cursor[1] + self.LINE_HEIGHT)
-        draw.line((width / 2, left_top[1]) + (width / 2, cursor[1]), fill=config.ACCENT, width=2)
+            right_bottom = (max(right_bottom[0], width / 2), max(right_bottom[1], cursor[1]))
+        draw.line((width / 2, left_top[1]) + (width / 2, cursor[1]), fill=config.ACCENT, width=1)
 
         if partial:
-            right_bottom = (width / 2, cursor[1])
+            # right_bottom = (width / 2, cursor[1])
             return image.crop(left_top + right_bottom), *left_top
         else:
             return image, 0, 0
@@ -105,7 +157,8 @@ class UpdateApp(App):
         pass
 
     def on_app_enter(self):
-        pass
+        self.__files_to_reset = self.__get_files_to_reset()
+        self.__files_to_clean = self.__get_files_to_clean()
 
     def on_app_leave(self):
         pass
