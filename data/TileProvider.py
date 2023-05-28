e2edfd4e47e9cd555f7dd403934750279f2b3da8
SirLefti <62506842+SirLefti@users.noreply.github.com>
2023-05-28 13:32:24 +0200

    documentation

--- data/TileProvider.py
+++ data/TileProvider.py
@@ -4,6 +4,7 @@ from PIL import Image
 
 
 class TileInfo:
+    """Representation of a map tile."""
 
     def __init__(self, top_left: Tuple[float, float], bottom_right: Tuple[float, float], image: Image):
         self.__top_left = top_left
@@ -12,25 +13,34 @@ class TileInfo:
 
     @property
     def top_left(self) -> Tuple[float, float]:
+        """Decimal degrees of the top left corner of the tile"""
         return self.__top_left
 
     @property
     def bottom_right(self) -> Tuple[float, float]:
+        """Decimal degrees of the bottom right corner of the tile"""
         return self.__bottom_right
 
     @property
     def image(self) -> Image:
+        """Tile image"""
         return self.__image
 
 
 class TileProvider(ABC):
+    """Data provider for map tiles."""
 
     @abstractmethod
     def get_tile(self, lat: float, lon: float, zoom: int, size: Tuple[int, int] = (256, 256), x_offset: int = 0,
                  y_offset: int = 0) -> TileInfo:
+        """
+        Returns a tile object for the given decimal degrees and zoom level with the requested position being in the
+        center.
+        """
         raise NotImplementedError
 
     @property
     @abstractmethod
     def zoom_range(self) -> Iterable[int]:
+        """Returns a range of supported zoom levels."""
         raise NotImplementedError
