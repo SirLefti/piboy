dec43d2b2f10334a65ece79b94075abd17327701
SirLefti <62506842+SirLefti@users.noreply.github.com>
2023-10-12 19:53:35 +0200

    bigger icons, arranged side by side

--- app/EnvironmentApp.py
+++ app/EnvironmentApp.py
@@ -46,12 +46,13 @@ class EnvironmentApp(SelfUpdatingApp):
     def draw(self, image: Image, partial=False) -> (Image, int, int):
         draw = ImageDraw.Draw(image)
         width, height = self.__resolution
-        icon_gap = 5
-        left_top = (self.__app_side_offset + 150, self.__app_top_offset + 50)
+        icon_gap = 10
+        left_top = ((width - self.__t_icon.width - self.__p_icon.width - self.__h_icon.width - 2 * icon_gap) // 2,
+                    self.__app_top_offset + 50)
 
         temperature_xy = left_top
-        pressure_xy = (temperature_xy[0], temperature_xy[1] + self.__t_icon.height)
-        humidity_xy = (pressure_xy[0], pressure_xy[1] + self.__p_icon.height)
+        pressure_xy = (temperature_xy[0] + self.__t_icon.width + icon_gap, temperature_xy[1])
+        humidity_xy = (pressure_xy[0] + self.__p_icon.width + icon_gap, pressure_xy[1])
 
         # only drawn once
         if not partial:
@@ -59,20 +60,31 @@ class EnvironmentApp(SelfUpdatingApp):
             draw.bitmap(pressure_xy, self.__p_icon, fill=self.__color)
             draw.bitmap(humidity_xy, self.__h_icon, fill=self.__color)
 
-        draw_area_left_top = (min(temperature_xy[0] + self.__t_icon.width,
-                              pressure_xy[0] + self.__p_icon.width,
-                              humidity_xy[0] + self.__h_icon.width), left_top[1])
+        draw_area_left_top = (left_top[0],
+                              min(temperature_xy[1] + self.__t_icon.height,
+                              pressure_xy[1] + self.__p_icon.height,
+                              humidity_xy[1] + self.__h_icon.height))
 
         # format with two decimals, and seven characters in total (adds whitespaces on the left to fill)
-        draw.text((temperature_xy[0] + self.__t_icon.width + icon_gap, temperature_xy[1]),
-                  f'{self.__data.temperature:.2f} °C', self.__color, font=self.__font)
-        draw.text((pressure_xy[0] + self.__p_icon.width + icon_gap, pressure_xy[1]),
-                  f'{self.__data.pressure:.2f} hPa', self.__color, font=self.__font)
-        draw.text((humidity_xy[0] + self.__h_icon.width + icon_gap, humidity_xy[1]),
-                  f'{self.__data.humidity:.2%}', self.__color, font=self.__font)
+        t_text = f'{self.__data.temperature:.2f} °C'
+        p_text = f'{self.__data.pressure:.2f} hPa'
+        h_text = f'{self.__data.humidity:.2%}'
+        _, _, t_text_width, t_text_height = self.__font.getbbox(t_text)
+        _, _, p_text_width, p_text_height = self.__font.getbbox(p_text)
+        _, _, h_text_width, h_text_height = self.__font.getbbox(h_text)
+        draw.text((temperature_xy[0] + (self.__t_icon.width - t_text_width) // 2,
+                   temperature_xy[1] + self.__t_icon.height + icon_gap),
+                  t_text, self.__color, font=self.__font)
+        draw.text((pressure_xy[0] + (self.__p_icon.width - p_text_width) // 2,
+                   pressure_xy[1] + self.__p_icon.height + icon_gap),
+                  p_text, self.__color, font=self.__font)
+        draw.text((humidity_xy[0] + (self.__h_icon.width - h_text_width) // 2,
+                   humidity_xy[1] + self.__h_icon.height + icon_gap),
+                  h_text, self.__color, font=self.__font)
 
         if partial:
-            right_bottom = width - self.__app_side_offset, humidity_xy[1] + self.__h_icon.height
+            right_bottom = (humidity_xy[0] + (self.__h_icon.width - h_text_width) // 2 + h_text_width,
+                            draw_area_left_top[1] + icon_gap + max(t_text_height, p_text_height, h_text_height))
             return image.crop(draw_area_left_top + right_bottom), *draw_area_left_top
         else:
             return image, 0, 0
